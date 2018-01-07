d304dbd68c0977bb03fdd4af78011f92a0587496
BinoyCherian <cherian2ams@gmail.com>
2018-01-07 20:43:09 +0100

    Fixed the error in the last commit

--- IamCore/src/fr/epita/iam/constants/Constants.java
+++ IamCore/src/fr/epita/iam/constants/Constants.java
@@ -28,16 +28,16 @@ public class Constants {
 	/** The constant for database url */
 	public static final String DB_URL = "URL";
 
-	/** The constatnt for the create operation */
+	/** The constant for the create operation */
 	public static final String CREATE_OPERATION = "create";
 
-	/** The constatnt for the read operation */
+	/** The constant for the read operation */
 	public static final String READ_OPERATION = "read";
 
-	/** The constatnt for the create operation */
+	/** The constant for the create operation */
 	public static final String UPDATE_OPERATION = "update";
 
-	/** The constatnt for the delete operation */
+	/** The constant for the delete operation */
 	public static final String DELETE_OPERATION = "delete";
 
 }
